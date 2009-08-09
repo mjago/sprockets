c72dcfa8399bb789d3593030d667c47149e27a6e
Martyn Jago <martyn.jago@btinternet.com>
2009-08-09 18:08:21 +0100

    changes to client.rb

--- lib/client.rb
+++ lib/client.rb
@@ -1,13 +1,58 @@
+require 'socket'
 
-require 'socket'      # Sockets are in standard library
-
-hostname = 'localhost'
-port = 2000
-
-s = TCPSocket.open(hostname, port)
-
-while line = s.gets   # Read lines from the socket
-  puts line.chop      # And print with platform line terminator
+def update_file(name,s)
+  File.open(name,"w") do |f|
+    while line = s.gets do
+      return if line.strip == '\0'
+      f.puts line.chop
+    end
+  end
 end
-s.close               # Close the socket when done
+
+@start_time = Time.new
+hostname = '192.168.1.64'
+port = 2000
+puts "hostname = #{hostname}"
+puts "port = #{port}"
+count = 0
+loop do
+  puts "looking for server"  
+  begin 
+
+    s = TCPSocket.open(hostname,port)
+
+    while line = s.gets do
+      @start_time = Time.now
+      if line.strip == 'tick'
+        STDOUT.print '.'
+        STDOUT.flush
+        count += 1
+        if count >= 5
+          count = 0
+          STDOUT.puts
+          STDOUT.flush
+        end
+      elsif line.include?('filename')
+        file_name = line[line.index('filename = ') + 'filename = '.length .. -2]
+        STDOUT.puts
+        STDOUT.puts "updating #{file_name}"
+        STDOUT.flush
+        update_file(file_name,s)
+        count = 0
+      end
+    end
+
+  rescue
+  
+    sleep 1
+
+  else
+    
+ end
+
+end
+
+STDOUT.puts "finished in #{Time.now - @start_time} seconds"
+
+exit
 
