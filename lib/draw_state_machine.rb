4171ef628075a4b79c48236560c17d4390982bcd
Martyn Jago <martyn.jago@btinternet.com>
2009-10-08 12:46:10 +0100

    modifications to graph drawing and StateData location

--- lib/draw_state_machine.rb
+++ lib/draw_state_machine.rb
@@ -1,30 +1,41 @@
+
 =begin rdoc
 = generate tertiary_state_machine
 =end
 
 def draw_state_machine
-	require File.expand_path(File.join(File.dirname(__FILE__),'..','..','..','docs','lib','graph_lib','graph_lib'))
-	require File.expand_path(File.join(File.dirname(__FILE__),'main_states'))
+	require File.expand_path(File.join(File.dirname(__FILE__),'graph_lib','graph_lib'))
+	require File.expand_path(File.join(File.dirname(__FILE__),'states'))
 	
 	@out_path = File.expand_path(File.join(File.dirname(__FILE__),''))
 	
 	gvr = GraphvizR.new 'draw_state_machine'
 	
-	gvr.graph[:label => "\n\nTertiary Main State Machine",
+	gvr.graph[:label => "\n\nMain State Machine",
 								:bgcolor => :white, 
 								:rankdir => "UD"
 							]
 	gvr.edge [:color=>:midnightblue, 
+								:color=>:darkgreen, 
+								:fontname => 'verdana',
+								:fontsize => '16',
+								:fontcolor => :darkgreen,
 								:url => 'http://google.com'
 	]
 	gvr.node [:color=>:black, 
+								:fontcolor => :navyblue,
+								:fontname => 'verdana',
+								:fontsize => '16',
 								:style=>:filled,
 								:fillcolor=>:lightblue,
 								:shape=>:box, 
 								:url => 'http://google.com'
 	]
 
-	States.main.each do |st|
+	#~ StateData.connection_state_data.each do |st|
+	StateData.tx_messaging_state_data.each do |st|
+	#~ StateData.rx_messaging_state_data.each do |st|
+	#~ StateData.main_state_data.each do |st|
 		gvr[st[0].to_s.to_sym] [:label => st[0].to_s.gsub("_","\n")]
 		(gvr[st[0].to_s.to_sym] >> gvr[st[2].to_s.to_sym])[:label => st[1].to_s.gsub("_","\n")]
 	end	
@@ -37,5 +48,5 @@ end
 
 if $0 ==__FILE__
 	draw_state_machine 
-	display_graph(File.expand_path(File.join(File.dirname(__FILE__), 'tertiary_state_machine.pdf'))) 
+	#display_graph(File.expand_path(File.join(File.dirname(__FILE__), 'tertiary_state_machine.pdf'))) 
 end	
--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -1,107 +1,107 @@
 
 require 'statemachine'
+require File.expand_path(File.join(File.dirname(__FILE__),'..','lib','states'))
+# class StateData
+#   attr_reader :test_state_data
+#   attr_reader :connection_state_data
+#   attr_reader :tx_messaging_state_data
+#   attr_reader :rx_messaging_state_data
+#   attr_reader :main_state_data
 
-class StateData
-  attr_reader :test_state_data
-  attr_reader :connection_state_data
-  attr_reader :tx_messaging_state_data
-  attr_reader :rx_messaging_state_data
-  attr_reader :main_state_data
-
-  def self.test_state_data
-    [
-     [:first_state, :first_action!, :second_state],
-    ]
-  end
+#   def self.test_state_data
+#     [
+#      [:first_state, :first_action!, :second_state],
+#     ]
+#   end
   
-  def self.connection_state_data
-    [
-     [:unconnected_state, :no_rx_detected!, :unconnected_state],
-     [:unconnected_state, :tx_detected!, :tx_only_state],
-     [:unconnected_state, :rx_detected!, :rx_only_state],
+#   def self.connection_state_data
+#     [
+#      [:unconnected_state, :no_rx_detected!, :unconnected_state],
+#      [:unconnected_state, :tx_detected!, :tx_only_state],
+#      [:unconnected_state, :rx_detected!, :rx_only_state],
      
-     [:tx_only_state, :tx_detected!, :tx_only_state],
-     [:tx_only_state, :rx_detected!, :full_duplex_state],
-     [:tx_only_state, :tx_dropped!, :unconnected_state],
+#      [:tx_only_state, :tx_detected!, :tx_only_state],
+#      [:tx_only_state, :rx_detected!, :full_duplex_state],
+#      [:tx_only_state, :tx_dropped!, :unconnected_state],
      
-     [:rx_only_state, :rx_detected!, :rx_only_state],
-     [:rx_only_state, :tx_detected!, :full_duplex_state],
-     [:rx_only_state, :rx_dropped!, :unconnected_state],
+#      [:rx_only_state, :rx_detected!, :rx_only_state],
+#      [:rx_only_state, :tx_detected!, :full_duplex_state],
+#      [:rx_only_state, :rx_dropped!, :unconnected_state],
      
-     [:full_duplex_state, :rx_detected!, :full_duplex_state],
-     [:full_duplex_state, :tx_detected!, :full_duplex_state],
-     [:full_duplex_state, :tx_dropped!, :rx_only_state],
-     [:full_duplex_state, :rx_dropped!, :tx_only_state],
-    ]
-  end
+#      [:full_duplex_state, :rx_detected!, :full_duplex_state],
+#      [:full_duplex_state, :tx_detected!, :full_duplex_state],
+#      [:full_duplex_state, :tx_dropped!, :rx_only_state],
+#      [:full_duplex_state, :rx_dropped!, :tx_only_state],
+#     ]
+#   end
   
-  def self.tx_messaging_state_data
-    [
-     [:idle_state, :no_action!, :idle_state],
-     [:idle_state, :send_file!, :sending_file_state],
-     [:idle_state, :delete_file!, :deleting_file_state],
-     [:idle_state, :send_structure!, :sending_structure_state],
-     [:idle_state, :send_hash!, :sending_hash_state],
-     [:idle_state, :send_tick!, :sending_tick_state],
+#   def self.tx_messaging_state_data
+#     [
+#      [:idle_state, :no_action!, :idle_state],
+#      [:idle_state, :send_file!, :sending_file_state],
+#      [:idle_state, :delete_file!, :deleting_file_state],
+#      [:idle_state, :send_structure!, :sending_structure_state],
+#      [:idle_state, :send_hash!, :sending_hash_state],
+#      [:idle_state, :send_tick!, :sending_tick_state],
      
-     [:sending_file_state, :no_action!, :send_file_state],
-     [:sending_file_state, :ack_received!, :idle_state],
-     [:sending_file_state, :nak_received!, :idle_state],
-     [:sending_file_state, :rx_timeout!, :idle_state],
+#      [:sending_file_state, :no_action!, :sending_file_state],
+#      [:sending_file_state, :ack_received!, :idle_state],
+#      [:sending_file_state, :nak_received!, :idle_state],
+#      [:sending_file_state, :rx_timeout!, :idle_state],
      
-     [:deleting_file_state, :no_action!, :deleting_file_state],
-     [:deleting_file_state, :ack_received!, :idle_state],
-     [:deleting_file_state, :nak_received!, :idle_state],
-     [:deleting_file_state, :rx_timeout!, :idle_state],
+#      [:deleting_file_state, :no_action!, :deleting_file_state],
+#      [:deleting_file_state, :ack_received!, :idle_state],
+#      [:deleting_file_state, :nak_received!, :idle_state],
+#      [:deleting_file_state, :rx_timeout!, :idle_state],
      
-     [:sending_structure_state, :no_action!, :sending_structure_state],
-     [:sending_structure_state, :ack_received!, :idle_state],
-     [:sending_structure_state, :nak_received!, :idle_state],
-     [:sending_structure_state, :rx_timeout!, :idle_state],
+#      [:sending_structure_state, :no_action!, :sending_structure_state],
+#      [:sending_structure_state, :ack_received!, :idle_state],
+#      [:sending_structure_state, :nak_received!, :idle_state],
+#      [:sending_structure_state, :rx_timeout!, :idle_state],
      
-     [:sending_tick_state, :no_action!, :sending_tick_state],
-     [:sending_tick_state, :ack_received!, :idle_state],
-     [:sending_tick_state, :nak_received!, :idle_state],
-     [:sending_tick_state, :rx_timeout!, :idle_state],
+#      [:sending_tick_state, :no_action!, :sending_tick_state],
+#      [:sending_tick_state, :ack_received!, :idle_state],
+#      [:sending_tick_state, :nak_received!, :idle_state],
+#      [:sending_tick_state, :rx_timeout!, :idle_state],
      
-     [:sending_hash_state, :no_action!, :sending_hash_state],
-     [:sending_hash_state, :ack_received!, :idle_state],
-     [:sending_hash_state, :nak_received!, :idle_state],
-     [:sending_hash_state, :rx_timeout!, :idle_state],
-    ]
-  end
+#      [:sending_hash_state, :no_action!, :sending_hash_state],
+#      [:sending_hash_state, :ack_received!, :idle_state],
+#      [:sending_hash_state, :nak_received!, :idle_state],
+#      [:sending_hash_state, :rx_timeout!, :idle_state],
+#     ]
+#   end
   
-  def self.rx_messaging_state_data
-    [
-     [:idle_state, :no_action!, :idle_state],
-     [:idle_state, :test_async_rx!, :testing_async_rx_state],
-     [:idle_state, :await_ack!, :awaiting_ack_state],
+#   def self.rx_messaging_state_data
+#     [
+#      [:idle_state, :no_action!, :idle_state],
+#      [:idle_state, :test_async_rx!, :testing_async_rx_state],
+#      [:idle_state, :await_ack!, :awaiting_ack_state],
 
-     [:testing_async_rx_state, :no_async_rx_data!, :idle_state],
-     [:testing_async_rx_state, :async_rx_data!, :idle_state],
-    ]
-  end
+#      [:testing_async_rx_state, :no_async_rx_data!, :idle_state],
+#      [:testing_async_rx_state, :async_rx_data!, :idle_state],
+#     ]
+#   end
   
-  def self.main_state_data
-    [
-     [:init_state, :initialised!, :contact_tester_state],
+#   def self.main_state_data
+#     [
+#      [:init_state, :initialised!, :contact_tester_state],
      
-     [:contact_tester_state, :tester_not_contacted!, :contact_tester_state],
-     [:contact_tester_state, :tester_contacted!, :listen_for_tester_state],
-     [:contact_tester_state, :contact_tester_timeout!, :init_state],
+#      [:contact_tester_state, :tester_not_contacted!, :contact_tester_state],
+#      [:contact_tester_state, :tester_contacted!, :listen_for_tester_state],
+#      [:contact_tester_state, :contact_tester_timeout!, :init_state],
 
-     [:listen_for_tester_state, :tester_unheard!, :listen_for_tester_state],
-     [:listen_for_tester_state, :tester_heard!, :send_tester_hash_state],
-     [:listen_for_tester_state, :tester_listening_timeout!, :contact_tester_state],
+#      [:listen_for_tester_state, :tester_unheard!, :listen_for_tester_state],
+#      [:listen_for_tester_state, :tester_heard!, :send_tester_hash_state],
+#      [:listen_for_tester_state, :tester_listening_timeout!, :contact_tester_state],
 
-     [:send_tester_hash_state, :sent_hash_to_tester!, :sent_tester_hash_state],
-     [:send_tester_hash_state, :retry_overcount!, :warning_hash_nak_overcount_state],
+#      [:send_tester_hash_state, :sent_hash_to_tester!, :sent_tester_hash_state],
+#      [:send_tester_hash_state, :retry_overcount!, :warning_hash_nak_overcount_state],
      
-     [:sent_tester_hash_state, :received_hash_ack!, :verify_tester_hash_state],
-     [:sent_tester_hash_state, :received_hash_nak!, :send_tester_hash_state],     
-    ]
-  end
-end
+#      [:sent_tester_hash_state, :received_hash_ack!, :verify_tester_hash_state],
+#      [:sent_tester_hash_state, :received_hash_nak!, :send_tester_hash_state],     
+#     ]
+#   end
+# end
 
 class StateMachines
   attr_accessor :connection_states
@@ -371,7 +371,7 @@ describe StateData do
         
         it 'is re-entrant given no_action!' do
           @statemachine.tx_message_states.no_action!
-          @statemachine.tx_message_states.state.should == :send_file_state
+          @statemachine.tx_message_states.state.should == :sending_file_state
         end
         
         it 'changes state to idle_state following ack_received!' do
