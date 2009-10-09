47ec4be9044c82c8fb6907f2e3ac80a39dd05a0d
Martyn Jago <martyn.jago@btinternet.com>
2009-10-09 14:09:02 +0100

    refactored main_state_event

--- lib/dev.rb
+++ lib/dev.rb
@@ -16,23 +16,31 @@ class Dev
 	  @states = StateMachines.new
     @states.build_state_machine('dev_main_state_data')
     @states.build_state_machine('connection_state_data')
-    @tx_port = TCPServer.open(2000)  # Socket to listen on port 2000
+    @tx_port = TCPServer.open(2000)
 		@timer = ProcessTimers.new
 		@timer.reset
 		@state_timer = 0
-		#~ @seconds_count = 0
-		#~ @time_started = Time.now
 	end
 	
 	def states
 		@states
   end
 
+	def main_state_event(new_event)
+		state_timer = 0
+		self.states.dev_main_states.send(new_event)
+		STDOUT.puts "#{new_event} event"
+		STDOUT.flush
+	end	
+
+	def main_state
+		self.states.dev_main_states.state
+	end
+	
   def tester_contacted?
     begin
       @tester_tx_socket = @tx_port.accept_nonblock
     rescue
-			#~ STDOUT.puts 'tester didn\'t respond'
       return false
     end
     true
@@ -53,87 +61,30 @@ class Dev
 	
 end
 
-#~ class DevSocket
-  #~ def contact_tester(port)
-    #~ @dev = TCPServer.open(port)  # Socket to listen on port 2000
-  #~ end
-  
-  #~ def tester_responded?
-    #~ begin
-      #~ @tester_tx_socket = @dev.accept_nonblock
-    #~ rescue
-      #~ sleep 1
-      #~ return false
-    #~ end
-    #~ STDOUT.puts 'tester responded'
-    #~ STDOUT.flush
-    #~ return true
-  #~ end
-
-  #~ def listen_for_tester
-    #~ loop do
-			#~ dev.process_timers
-      #~ STDOUT.puts "looking for tester"
-      #~ STDOUT.flush
-      #~ begin
-        #~ s = TCPSocket.open('192.168.10.91',3001)
-        
-        #~ while line = s.gets do
-          #~ puts line
-        #~ end
-      #~ end
-			sleep 1
-		#~ end
-  #~ end
-#~ end
-
 if $0 == __FILE__
   dev = Dev.new
 	loop do
 		dev.process_timers
-		#~ STDOUT.puts dev.states.dev_main_states.state
-		#~ STDOUT.flush
-		case dev.states.dev_main_states.state
+		case dev.main_state
 			when :init_state
-				#~ puts 'dev_main_state = init_state'
-				dev.states.dev_main_states.initialised!
-				dev.state_timer = 0
-				STDOUT.puts 'initialised! event'
-				STDOUT.flush
+				dev.main_state_event(:initialised!)
 			when :contact_tester_state
-				#~ puts 'dev_main_state = contact_tester_state'
 				if dev.tester_contacted?
-					dev.state_timer = 0.0
-					dev.states.dev_main_states.tester_contacted!
-					dev.states.connection_states.tx_detected!
-					STDOUT.puts 'tester_contacted! event'
-					STDOUT.flush
+					dev.main_state_event(:tester_contacted!)
 				elsif dev.state_timer >= 5.0
-					dev.state_timer = 0.0
-					dev.states.dev_main_states.contact_tester_timeout!
-					STDOUT.puts 'contact_tester_timeout! event'
-					STDOUT.flush
+					dev.main_state_event(:contact_tester_timeout!)
 				end
 				
 			when :listen_for_tester_state
 				if dev.listen_for_tester?
-					dev.state_timer = 0.0
-					dev.states.dev_main_states.tester_heard!
-					STDOUT.puts 'tester_heard! event'
-					STDOUT.flush
+					dev.main_state_event(:tester_heard!)
 				elsif dev.state_timer >= 5.0
-					dev.state_timer = 0.0
-					dev.states.dev_main_states.tester_listening_timeout!
-					STDOUT.puts 'tester_listening_timeout! event'
-					STDOUT.flush
+					dev.main_state_event(:tester_listening_timeout!)
 				end
 				
 			when :send_tester_tick_state
 				dev.tester_tx_socket.puts 'tick'
-				dev.state_timer = 0.0
-				dev.states.dev_main_states.sent_tick_to_tester!
-				STDOUT.puts 'sent_tick_to_tester! event'
-				STDOUT.flush
+				dev.main_state_event(:sent_tick_to_tester!)
 				
 			when :await_tick_ack_state
 				@message = dev.tester_rx_socket.gets
@@ -142,25 +93,16 @@ if $0 == __FILE__
 					if @message.include?('tick_ack')
 						dev.state_timer = 0.0
 						@message = ''
-						dev.states.dev_main_states.received_tick_ack!
-						STDOUT.puts 'await_tick_timeout! event'
-						STDOUT.flush
+						dev.main_state_event(:received_tick_ack!)
 					end
 				elsif dev.state_timer >= 3
-					dev.state_timer = 0.0
 					@message = ''
-					dev.states.dev_main_states.await_tick_timeout!
-					STDOUT.puts 'await_tick_timeout! event'
-					STDOUT.flush
+					dev.main_state_event(:await_tick_timeout!)
 				end
-				
 				
 			else
 				puts "ERROR! Unknown dev_main_state #{dev.states.dev_main_states.state}"
 			
 		end
-		#~ sleep 1
 	end	
 end
-
-
